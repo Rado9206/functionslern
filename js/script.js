1aea5fa314d3d41a14bd523b0f40180925e5d3a6
Radek <rj.airoo@gmail.com>
2023-04-12 19:01:39 +0200

    added fuction if

--- js/script.js
+++ js/script.js
@@ -10,3 +10,19 @@ const odd = () => console.log(`Liczba ${score} jest nieparzysta.`)
 
 add(0, 2)
 
+console.log('----------')
+
+const num = 10
+const numbers = []
+
+for (let i = 0; i < num; i++) {
+	numbers.push(i)
+}
+
+const checkNumbers = number => {
+	if (number % 3 === 0 && number !== 0) {
+		console.log(`${number} jest podzielne przez 3`)
+	} else console.log(`${number} jest niepodzielne przez 3 albo wynosi 0.`)
+}
+
+numbers.forEach(checkNumbers)
